64ad1bed40db2e5f03c3768200d6388f8585e105
mirzoevbakhtiarr <baxtiyarm507@gmail.com>
2023-07-13 03:48:58 -0800

    Added

---
+++ 02.04/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace  std;
+#pragma region Task 1
+//const int LengthChar(char* str);
+//void edit(char* str, char* old_w, char* new_w, int length);
+//int main()
+//{
+//	char text[]{ "From above, this rivers delta appears very much like a tree of life." };
+//	cout << text << endl;
+//	char word[10]{};
+//	char wordText[10]{};
+//	int length = LengthChar(text);
+//	cout << "What word to change: " << endl;
+//	cin >> wordText;
+//	cout << "Enter new word" << endl;
+//	cin >> word;
+//	edit(text, wordText, word, length);
+//    cout <<text;
+//	return 0;
+//}
+#pragma endregion
+#pragma region Task 2
+//const int LengthChar(char* str);
+//void point(char* str, int length);
+//int main()
+//{
+//	char text[]{ "From above, this. rivers delta appears. very much like a tree of life." };
+//	int length = LengthChar(text);
+//	text[length - 1] = '\0';
+//	point(text, length);
+//	cout << text;
+//	return 0;
+//}
+#pragma endregion
+#pragma region Task 3
+//const int LengthChar(char* str);
+//void lowerCase(char* str, int length);
+//void letterCount(char* str, int* ar, int length);
+//int main()
+//{
+//	char a = 'a';
+//	int arr[26]{};
+//	char text[]{ "From above, this rivers delta appears very much like a tree of life." };
+//	int length = LengthChar(text);
+//	cout << text << endl;
+//	lowerCase(text,length);
+//	letterCount(text, arr, length);
+//	for (size_t i = 0; i < 26; i++)
+//	{
+//		if (arr[i] != 0)
+//		{
+//			cout << a << "=" << arr[i] << endl;
+//		}
+//		a++;
+//	}
+//	return 0;
+//}
+#pragma endregion
+#pragma region Task 4
+//const int LengthChar(char* str);
+//int findNum(char* str, int length, int count);
+//int main()
+//{
+//	char text[]{ "From above, this rivers delta appears very much like a tree of life." };
+//	int length = LengthChar(text);
+//	int numCount{};
+//	cout << "The number of digits in the text is: " << findNum(text, length, numCount);
+//
+//	return 0;
+//}
+#pragma endregion
